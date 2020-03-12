30b520e413b394139eaa8a07aca257f03148c9e0
Mehrdad Zade <mehrdad.azh@gmail.com>
2020-03-12 16:56:06 -0400

    correcting level2 encryption for admin login and compose only after login

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 //jshint esversion:6
-
+require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
+const encrypt = require("mongoose-encryption");
 
 const homeStartingContent = "You can find a summary of my daily reads and thoughts here";
 const aboutContent = "I'm a computer scientist, husband, father and IT geek who follows the stock market and has a lot of curiosity about finding a relevant meditation with the goal of happiness and well-being";
@@ -15,7 +16,7 @@ app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
-const uri = "mongodb+srv://admin-mehrdad:Test123@mongodbcluster-qiqmh.mongodb.net/blogDB";
+const uri = "mongodb+srv://" + process.env.ADMIN + ":" + process.env.PASSWORD + "@mongodbcluster-qiqmh.mongodb.net/blogDB";
 //mongoose.connect(uri, {useNewUrlParser: true});
 mongoose.connect(uri, function(err, client) {
    if(err) {
@@ -31,8 +32,14 @@ const postSchema = {
   title: String,
   content: String
 };
-
 const Post = mongoose.model("Post", postSchema);
+
+const userSchema = new mongoose.Schema({
+  email : String,
+  password : String
+});
+userSchema.plugin(encrypt, { secret : process.env.KEY , encryptedFileds : ["password"]});
+const User = mongoose.model("User", userSchema);
 
 app.get("/", function(req, res){
   Post.find({}, function(err, posts){
@@ -43,8 +50,9 @@ app.get("/", function(req, res){
   });
 });
 
+//we don't want access to compose unless people are logged in
 app.get("/compose", function(req, res){
-  res.render("compose");
+  res.send("Please login..");
 });
 
 app.post("/compose", function(req, res){
@@ -82,6 +90,39 @@ app.get("/contact", function(req, res){
   res.render("contact", {contactContent: contactContent});
 });
 
+app.get("/login", function(req, res){
+  res.render("login");
+});
+
+app.post("/login", function(req, res){
+  User.findOne({email : req.body.username }, function(err, foundUser){
+    if(foundUser && foundUser.password === req.body.password){
+      res.render("compose");
+    }else{
+      res.send("Username and Password does not match!");
+    }
+  })
+});
+
+//for now only admin is registered
+
+app.get("/register", function(req, res){
+  res.render("register");
+});
+
+app.post("/register", function(req, res){
+  const user = new User({
+    email : req.body.username,
+    password : req.body.password
+  });
+  user.save(function(err){
+      if (!err){
+          res.redirect("/");
+      }else{
+        console.log(err);
+      }
+    });
+});
 
 app.listen(process.env.PORT || 3000, function() {
   console.log("###Server started on port 3000");
